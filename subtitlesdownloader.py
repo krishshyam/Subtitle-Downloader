d9e4efeb760cccd19b0373cfe2e0752b62ac3c28
krishshyam <krishshyam007@gmail.com>
2016-05-20 21:42:08 +0530

    Error case and language selection added

--- subtitlesdownloader.py
+++ subtitlesdownloader.py
@@ -35,15 +35,42 @@ r  = requests.get(url+tvurl)
 data = r.text   
 soup = BeautifulSoup(data)
 
-sea=input("Enter the season")
-epi=input("Enter the episode")
-ver=input("Enter the version")
+sea=input("Enter the season: ")
+epi=input("Enter the episode: ")
+ver=input("Enter the version: ")
+lang=input("Enter the language: ")
 answer= ""
 soup = BeautifulSoup(data)
 tag = soup.findAll('td')
-for i in range(7, 1000, 11):
-    if(tag[i].text==sea and tag[i+1].text==epi and tag[i+3].text=="English" and tag[i+4].text==ver):
+for i in range(7, 10000, 11):
+    if(tag[i].text==sea and tag[i+1].text==epi and tag[i+3].text==lang and tag[i+4].text==ver):
         tag_a=tag[i+9].find('a')
         answer = tag_a['href']
         break
-os.system("start IDMan /d "+url+answer)
+    if(tag[i+1] is tag[-1]):
+        break
+    if(tag[i+2] is tag[-1]):
+        break
+    if(tag[i+3] is tag[-1]):
+        break
+    if(tag[i+4] is tag[-1]):
+        break
+    if(tag[i+5] is tag[-1]):
+        break
+    if(tag[i+6] is tag[-1]):
+        break
+    if(tag[i+7] is tag[-1]):
+        break
+    if(tag[i+8] is tag[-1]):
+        break
+    if(tag[i+9] is tag[-1]):
+        break
+    if(tag[i+10] is tag[-1]):
+        break
+    if(tag[i+11] is tag[-1]):
+        break
+if(answer==""):
+    print("\n\n\nNo Subtitle Found")
+if(answer!=""):
+    print("\n\nYay!")
+    os.system("start IDMan /d "+url+answer)
